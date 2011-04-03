7102401a656e0992d2467a7d5265c5677246fb75
toddmazierski <toddmazierski@gmail.com>
2011-04-03 18:31:57 -0400

    Initial import of Black Box Software Testing Puzzle from a different repository

---
+++ black-box-testing-puzzle.rb
@@ -0,0 +1,32 @@
+require 'rubygems'
+require 'sinatra'
+require 'currency'
+require 'erb'
+
+$new_bank_rate_percent = 2.50
+$new_bank_name = 'The Royal Bank of Sealand'
+
+def compound_interest(principal, rate_percent, years)
+  principal * (1 + (rate_percent / 100)) ** years
+end
+
+get '/' do
+  erb :index
+end
+
+post '/compare' do
+  @current_principal = params[:current_principal].to_f
+  @current_rate_percent = params[:current_rate_percent].to_f
+  @years = params[:years].to_f
+  
+  @current_bank_principal = Currency::Money(compound_interest(@current_principal, @current_rate_percent, @years), :USD)
+  @new_bank_principal = Currency::Money(compound_interest(@current_principal, $new_bank_rate_percent, @years), :USD)
+  
+  @difference = @new_bank_principal - @current_bank_principal
+  
+  erb :result
+end
+
+get '/instructions' do
+  erb :instructions
+end
